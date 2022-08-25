1980ca7f4b30ad8e3ed4099f436263c3e9f316a1
AlexeyEkimow <91189404+AlexeyEkimow@users.noreply.github.com>
2022-08-25 15:15:00 +0300

    Initial Commit

---
+++ totall Bill/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  totall Bill
+//
+//  Created by Alexey Ekimow on 25.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
